a9f8d1e201878373f142dacfce01e02911da2989
Sławomir Bąk <slawek417@gmail.com>
2022-01-03 23:12:49 +0100

    create backbone for test

--- BookstoreSPA/src/app/views/books/book-test-form/book-test-form.component.ts
+++ BookstoreSPA/src/app/views/books/book-test-form/book-test-form.component.ts
@@ -1,5 +1,9 @@
+import { forEach } from '@angular-devkit/schematics';
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { IQuestion } from 'src/app/core/models/IQuestion';
+import { ITest } from 'src/app/core/models/ITest';
 import { KnowledgeService } from 'src/app/core/services/knowledge.service';
 
 @Component({
@@ -9,11 +13,73 @@ import { KnowledgeService } from 'src/app/core/services/knowledge.service';
 })
 export class BookTestFormComponent implements OnInit {
 
-  constructor(private knowledgeService: KnowledgeService, private route: ActivatedRoute) { }
+  qustionsForm: FormGroup;
 
-  test$ = this.knowledgeService.getSimle(`test/${+this.route.snapshot.paramMap.get('id')}`);
+  constructor(private knowledgeService: KnowledgeService, private route: ActivatedRoute, public formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
+  test: ITest;
+  likes = 0;
+  disLike = 0;
+
+  get questions(): FormArray{
+    return this.qustionsForm.get('questions') as FormArray;
   }
 
+  ngOnInit(): void {
+    this.knowledgeService.getSimle(`test/${+this.route.snapshot.paramMap.get('id')}`).subscribe(test => {
+      this.test = test;
+
+      console.log("on init", this.test);
+
+      test.questions.forEach(question => {
+        this.questions.push(this.buildQuestions(question));
+      });
+
+      this.crateStatistics(test);
+    });
+
+    this.qustionsForm = this.formBuilder.group({
+      questions:  this.formBuilder.array([]),
+    });
+  }
+
+  private buildQuestions(question: IQuestion): FormGroup {
+    return this.formBuilder.group({
+      answer: ['', Validators.required]
+    });
+  }
+
+  checkTest() {
+    this.qustionsForm.markAllAsTouched();
+
+    if (!this.qustionsForm.valid) {
+      return;
+    }
+
+    this.test.questions.forEach((question, index) => {
+      question.answer = this.qustionsForm.value.questions[index].answer;
+    });
+
+    console.log("send", this.test);
+  }
+
+  hasError = (controlName: string, errorName: string ): boolean  =>{
+    const field = (this.qustionsForm.controls.questions as any).controls[controlName].controls.answer;
+    if (field.touched) {
+      return field.hasError(errorName);
+    }
+    return false;
+  }
+
+  likeTest = (like: boolean) => {
+    console.log("hot or not?", like);
+  }
+  crateStatistics = (test: ITest) => {
+    let likes = 0;
+    let dislike = 0;
+
+    test.likes.forEach(like => like.isLike ? likes++ : dislike++);
+    this.likes = likes;
+    this.disLike = dislike;
+  }
 }
